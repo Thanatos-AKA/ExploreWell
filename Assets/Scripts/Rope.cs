3416524cc6c06ce97deaee855d07fee63e77dd87
Thanatos-AKA <61469885+Thanatos-AKA@users.noreply.github.com>
2021-03-05 17:42:42 +0800

    complete rope script

--- Assets/Scripts/Rope.cs
+++ Assets/Scripts/Rope.cs
@@ -3,53 +3,112 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rope : MonoBehaviour{
-    //prefab rope and list of rope
+    public Rigidbody2D connectedObject;
     public GameObject ropeSegmentPrefab;
     List<GameObject> ropeSegments = new List<GameObject>();
 
-    //if the rope is incresing or decreasing
+    public bool isDecreasing{get; set;}
     public bool isIncreasing{get; set;}
-    public bool isDecreasing{get; set;} 
 
-    //the rigidbody which connects to the end of rope
-    public Rigidbody2D connectedObject;
-
-    //the maximum length of rope (add another pieces when it get too long)
     public float maxRopeSegmentLength = 1.0f;
-
-    //casting speed
     public float ropeSpeed = 4.0f;
 
-    //rope's renderer
     LineRenderer lineRenderer;
 
-    //destroy all the rope segments and use CreateRopeSegment
-    public vois ResetLength(){
-        foreach (GameObject segment in ropeSegments){
+    void Start(){
+        lineRenderer = GetComponent<LineRenderer>();
+        ResetLength();
+    }
+
+    public void ResetLength(){
+        foreach(GameObject segment in ropeSegments){
             Destroy(segment);
         }
 
         ropeSegments = new List<GameObject>();
-        isDecreasing = false;
         isIncreasing = false;
+        isDecreasing = false;
+
         CreateRopeSegment();
     }
 
     void CreateRopeSegment(){
-        //create new segment
         GameObject segment = (GameObject)Instantiate(ropeSegmentPrefab, this.transform.position, Quaternion.identity);
-
-        //make the new segment be the child of this object
-        //remain it's "world position"
         segment.transform.SetParent(this.transform, true);
+        Rigidbody2D segmentBody = segment.GetComponent<Rigidbody2D>();
+        SpringJoint2D segmentJoint = segment.GetComponent<SpringJoint2D>();
 
+        if(segmentBody == null || segmentJoint == null){
+            Debug.LogError("Rope segment body prefab has no Rigidbody2D and/or SpringJoint2D!");
+        }
+
+        ropeSegments.Insert(0, segment);
+
+        if(ropeSegments.Count == 1){
+            SpringJoint2D connectedObjectJoint = connectedObject.GetComponent<SpringJoint2D>();
+            connectedObjectJoint.connectedBody = segmentBody;
+            connectedObjectJoint.distance = 0.1f;
+
+            segmentJoint.distance = maxRopeSegmentLength;
+        }
+        else{
+            GameObject nextSegment = ropeSegments[1];
+            SpringJoint2D nextSegmentJoint = nextSegment.GetComponent<SpringJoint2D>();
+
+            nextSegmentJoint.connectedBody = segmentBody;
+            segmentJoint.distance = 0.0f;
+        }
+
+        segmentJoint.connectedBody = this.GetComponent<Rigidbody2D>();
     }
 
-    void Start(){
-        
+    void RemoveRopeSegment(){
+        if(ropeSegments.Count < 1){
+            return;
+        }
+
+        GameObject topSegment = ropeSegments[0];
+        GameObject nextSegment = ropeSegments[1];
+
+        SpringJoint2D nextSegmentJoint = nextSegment.GetComponent<SpringJoint2D>();
+        nextSegmentJoint.connectedBody = this.GetComponent<Rigidbody2D>();
+
+        ropeSegments.RemoveAt(0);
+        Destroy(topSegment);
     }
 
     void Update(){
-        
+        GameObject topSegment = ropeSegments[0];
+        SpringJoint2D topSegmentJoint = topSegment.GetComponent<SpringJoint2D>();
+
+        if(isIncreasing){
+            if(topSegmentJoint.distance >= maxRopeSegmentLength){
+                CreateRopeSegment();
+            }
+            else{
+                topSegmentJoint.distance += ropeSpeed * Time.deltaTime;
+            }
+        }
+
+        if(isDecreasing){
+            if(topSegmentJoint.distance <= 0.005f){
+                RemoveRopeSegment();
+            }
+            else{
+                topSegmentJoint.distance -= ropeSpeed * Time.deltaTime;
+            }
+        }
+
+        if(lineRenderer != null){
+            lineRenderer.positionCount = ropeSegments.Count + 2;
+            lineRenderer.SetPosition(0, this.transform.position);
+
+            for(int i = 0; i < ropeSegments.Count; i++){
+                lineRenderer.SetPosition(i + 1, ropeSegments[i].transform.position);
+            }
+
+            SpringJoint2D connectedObjectJoint = connectedObject.GetComponent<SpringJoint2D>();
+            lineRenderer.SetPosition(ropeSegments.Count + 1, connectedObject.transform.TransformPoint(connectedObjectJoint.anchor));
+        }
     }
 }
